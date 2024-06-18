2135f4469a870581849daecf34738e42e6195f55
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-06-18 23:21:32 +0200

    feat: hides tutorials on searchPoisView if hideTutorials selected

--- src/views/SearchViews/searchPoisView.vue
+++ src/views/SearchViews/searchPoisView.vue
@@ -1,7 +1,7 @@
 <template>
   <BackgroundGradient />
 
-  <blurEffect v-if="searchPoiPopup"></blurEffect>
+  <blurEffect v-if="searchPoiPopup && !hideTutorials"></blurEffect>
 
   <header>
     <div class="header-buttons">
@@ -10,7 +10,7 @@
     </div>
   </header>
   <!--  INFO POPUP für den standort bestimmungsbutton -->
-  <div class="searchpoi-popup-container" v-if="searchPoiPopup">
+  <div class="searchpoi-popup-container" v-if="searchPoiPopup && !hideTutorials">
     <svg
       class="searchpoi-svg-popup"
       viewBox="0 0 100 100"
@@ -135,6 +135,11 @@ export default {
     return {
       store: storeData(),
       searchPoiPopup: true
+    }
+  },
+  computed: {
+    hideTutorials() {
+      return localStorage.getItem('hideTutorials') === 'true'
     }
   },
   mounted() {
